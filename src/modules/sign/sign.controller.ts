525ae0432fe8112f9f0a4840e1118666c3ae0a9f
Felipe Fernandes <f.fernandescs@gmail.com>
2026-03-29 20:03:16 -0300

    dsa

--- src/modules/sign/sign.controller.ts
+++ src/modules/sign/sign.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post, BadRequestException, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { SignService } from './sign.service';
+import { Public } from '../../common/decorators/public.decorator';
 
 @Controller('sign')
 export class SignController {
   constructor(private readonly signService: SignService) {}
 
+  @Public()
   @Post()
   sign(@Body('data') data: string, @Res() res: Response) {
     if (!data) {
