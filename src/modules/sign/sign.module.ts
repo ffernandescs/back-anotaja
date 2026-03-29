48fbd621ed90b58cd5ff627c6bedee7cdca97a5b
Felipe Fernandes <f.fernandescs@gmail.com>
2026-03-29 17:55:43 -0300

    das

--- src/modules/sign/sign.module.ts
+++ src/modules/sign/sign.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { SignController } from './sign.controller.ts';
+import { SignController } from './sign.controller';
 import { SignService } from './sign.service';
 
 @Module({
