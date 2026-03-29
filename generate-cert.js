e1b1c1c35023ae9d4e0e98bce6429b0fcb24aad1
Felipe Fernandes <f.fernandescs@gmail.com>
2026-03-29 19:51:02 -0300

    dsads

--- generate-cert.js
+++ generate-cert.js
@@ -1,7 +1,7 @@
-const { generateKeyPairSync } = require('crypto');
+const { generateKeyPairSync, createSign } = require('crypto');
 const fs = require('fs');
 
-// Gerar chave
+// Gerar chave RSA
 const { privateKey, publicKey } = generateKeyPairSync('rsa', {
   modulusLength: 2048,
 });
@@ -12,13 +12,43 @@ const privateKeyPem = privateKey.export({
   format: 'pem',
 });
 
-// Exportar certificado (simples)
-const publicKeyPem = publicKey.export({
+// Criar certificado X.509 autoassinado
+const forge = require('node-forge');
+
+// Converter chave para formato Forge
+const privateKeyForge = forge.pki.privateKeyFromPem(privateKeyPem);
+
+// Criar certificado
+const cert = forge.pki.createCertificate();
+cert.publicKey = forge.pki.publicKeyFromPem(publicKey.export({
   type: 'spki',
   format: 'pem',
-});
+}));
 
-fs.writeFileSync('private-key.pem', privateKeyPem);
-fs.writeFileSync('certificate.pem', publicKeyPem);
+cert.serialNumber = '01';
+cert.validity.notBefore = new Date();
+cert.validity.notAfter = new Date();
+cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 10);
 
-console.log('✅ Certificados gerados!');
+// Atributos do certificado
+const attrs = [{
+  name: 'commonName',
+  value: 'anotaja.shop'
+}, {
+  name: 'organizationName',
+  value: 'Anotaja'
+}, {
+  shortName: 'OU',
+  value: 'Anotaja'
+}];
+
+cert.setSubject(attrs);
+cert.setIssuer(attrs);
+
+// Assinar certificado
+cert.sign(privateKeyForge);
+
+fs.writeFileSync('src/keys/private-key.pem', privateKeyPem);
+fs.writeFileSync('certificate.pem', forge.pki.certificateToPem(cert));
+
+console.log('✅ Certificado X.509 e chave privada gerados!');
