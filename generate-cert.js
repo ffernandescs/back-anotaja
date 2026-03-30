c16c79b28b8c2dfa137853073ea5bc903e357e63
Felipe Fernandes <f.fernandescs@gmail.com>
2026-03-29 21:03:53 -0300

    ddas

--- generate-cert.js
+++ generate-cert.js
@@ -45,10 +45,53 @@ const attrs = [{
 cert.setSubject(attrs);
 cert.setIssuer(attrs);
 
+// Adicionar SAN (Subject Alternative Name) para QZ Tray
+const sans = [
+  {
+    type: 2, // DNS name
+    value: 'anotaja.shop'
+  },
+  {
+    type: 2,
+    value: 'www.anotaja.shop'
+  },
+  {
+    type: 2,
+    value: 'localhost'
+  },
+  {
+    type: 7, // IP address
+    ip: '127.0.0.1'
+  }
+];
+
+cert.setExtensions([{
+  name: 'subjectAltName',
+  altNames: sans
+}, {
+  name: 'basicConstraints',
+  cA: false
+}, {
+  name: 'keyUsage',
+  keyCertSign: true,
+  digitalSignature: true,
+  nonRepudiation: true,
+  keyEncipherment: true,
+  dataEncipherment: true
+}, {
+  name: 'extKeyUsage',
+  serverAuth: true,
+  clientAuth: true
+}]);
+
 // Assinar certificado
 cert.sign(privateKeyForge);
 
 fs.writeFileSync('src/keys/private-key.pem', privateKeyPem);
 fs.writeFileSync('certificate.pem', forge.pki.certificateToPem(cert));
 
-console.log('✅ Certificado X.509 e chave privada gerados!');
+// Também salvar para frontend
+fs.writeFileSync('../web-entregaja/public/cert/digital-certificate.txt', forge.pki.certificateToPem(cert));
+
+console.log('✅ Certificado X.509 com SAN gerado para QZ Tray!');
+console.log('📋 Domínios: anotaja.shop, www.anotaja.shop, localhost, 127.0.0.1');
